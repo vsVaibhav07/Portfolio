8562d11920796712784f0a176abc3c7377f98c44
Vaibhav Shukla <vaibhavshuklamdi@gmail.com>
2025-09-04 01:51:51 +0530

    Updated screen width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,9 @@ import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
-    <div className="bg-black  w-full min-h-screen  text-white  ">
+    <div className="bg-black  w-screen min-h-screen  text-white  ">
       
-      <div className="bg-blue-950/50   relative rounded-s-sm w-full h-full min-h-screen   ">
+      <div className="bg-blue-950/50   relative rounded-s-sm w-screen h-full min-h-screen   ">
         <Navbar  />
         <div className="flex flex-col gap-1">
           <Hero  />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = () => {
   const [active, setActive] = useState(navLinks[0].name);
 
   return (
-    <header className="fixed top-0 z-50 w-full bg-gradient-to-r from-blue-950/90 to-violet-500/40 backdrop-blur-3xl text-white shadow-md">
+    <header className="fixed top-0 z-50 w-screen bg-gradient-to-r from-blue-950/90 to-violet-500/40 backdrop-blur-3xl text-white shadow-md">
       <div className="max-w-7xl mx-auto flex items-center justify-between p-4 sm:px-6">
         <h1 className="text-2xl font-serif font-bold">Portfolio</h1>
 
