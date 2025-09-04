ed52e8d1ee55174181a577f83412720882180fec
Vaibhav Shukla <vaibhavshuklamdi@gmail.com>
2025-09-04 15:58:29 +0530

    Updated nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Hero from "@/components/Hero";
-import Navbar from "@/components/Navbar";
+// import Navbar from "@/components/Navbar";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 
@@ -10,7 +10,7 @@ import Skills from "@/components/Skills";
 export default function Home() {
   return (
     <main className="bg-black flex flex-col gap-1 w-full text-white">
-      <Navbar />
+      {/* <Navbar /> */}
       <Hero />
       <About />
       <Skills />
